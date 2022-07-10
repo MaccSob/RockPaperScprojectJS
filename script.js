1df6a3a40e24dcec48cbd2a87b040c6ef4ecc425
Macc71 <71sobmaciej@gmail.com>
2022-07-10 23:19:00 +0200

    added scorecounter

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-var random = ["rock", "paper", "scissors"];
-var playerSelection = prompt("Choose your weapon!")
-var computerSelection =  [Math.floor(Math.random()* random.length)];
+let playerSelection = prompt("Choose your weapon!")
+let computerSelection;
+let pcScore = 0;
+let score = 0;
     function computerPlay() {
+        var random = ["rock", "paper", "scissors"];
         return random = [Math.floor(Math.random()* random.length)];
 }
 function playRound(playerSelection, computerSelection) {
-if(playerSelection === computerSelection) {
-    alert("Tie!")
-}
-else if (computerSelection == "rock" && playerSelection == "scissors") {
-(computerSelection == "scissors" && playerSelection == "paper") ||
-(computerSelection == "paper" && playerSelection == "rock") 
-alert("PC Wins!")
+    computerSelection = computerPlay();
+    playerSelection = playerSelection;
+    score = score;
+    pcScore = pcScore;
+
+if (computerSelection === playerSelection) {
+    return "Tie";
+    }
+ else if (computerSelection == "rock" && playerSelection == "scissors") {
+        (computerSelection == "scissors" && playerSelection == "paper") ||
+        (computerSelection == "paper" && playerSelection == "rock") 
+        pcScore =+ 1;
+        return "PC wins";
 }
 else if (computerSelection == "rock" && playerSelection == "paper")  {
 (computerSelection == "paper" && playerSelection == "scissors") ||
-(computerSelection == "rock" && playerSelection == "paper") 
-alert("You win!");
+(computerSelection == "rock" && playerSelection == "paper")
+score =+ 1;
+ return "You win";
 }
+
+
 }
-console.log(computerSelection);
-playRound();
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+        console.log("Your score is : " + score);
+        console.log("Computer score is : " + pcScore);
+     }
+     
+}
+game();
