fa8b5d44963287ce55c419c1d59f5573333afec6
Macc71 <71sobmaciej@gmail.com>
2022-07-13 23:19:19 +0200

    almost done here

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
-let playerSelection = prompt("Choose your weapon!")
-let computerSelection;
+const random = ["rock", "paper", "scissors"];
 let pcScore = 0;
 let score = 0;
+const playerSelection = prompt("Rock,paper,scissors?");
+const computerSelection = computerPlay();
     function computerPlay() {
-        var random = ["rock", "paper", "scissors"];
-        return random = [Math.floor(Math.random()* random.length)];
-}
-function playRound(playerSelection, computerSelection) {
-    computerSelection = computerPlay();
-    playerSelection = playerSelection;
+        return random[~~(Math.random() * random.length)];
+    }
+function playRound(computerSelection, playerSelection) {
     score = score;
     pcScore = pcScore;
 
 if (computerSelection === playerSelection) {
-    return "Tie";
+    alert("Tie");
     }
  else if (computerSelection == "rock" && playerSelection == "scissors") {
         (computerSelection == "scissors" && playerSelection == "paper") ||
         (computerSelection == "paper" && playerSelection == "rock") 
         pcScore =+ 1;
-        return "PC wins";
+        alert("PC wins");
 }
 else if (computerSelection == "rock" && playerSelection == "paper")  {
 (computerSelection == "paper" && playerSelection == "scissors") ||
 (computerSelection == "rock" && playerSelection == "paper")
-score =+ 1;
- return "You win";
+     score =+ 1;
+    alert("You win");
 }
 
 
 }
 function game() {
     for (let i = 0; i < 5; i++) {
-        playRound();
+        playRound(playerSelection, computerPlay());
         console.log("Your score is : " + score);
         console.log("Computer score is : " + pcScore);
      }
      
 }
+console.log(computerPlay());
+console.log(playRound(playerSelection, computerSelection));
 game();
+
