e41af2308c31255142d0b3b83f7f8103c995cc61
Macc71 <71sobmaciej@gmail.com>
2022-07-16 23:11:56 +0200

    done for now

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-const random = ["rock", "paper", "scissors"];
+let playerSelection;
+let computerSelection;
 let pcScore = 0;
 let score = 0;
-const playerSelection = prompt("Rock,paper,scissors?");
-const computerSelection = computerPlay();
+let randomArray = ['rock', 'paper', 'scissors']
     function computerPlay() {
-        return random[~~(Math.random() * random.length)];
+        let random = randomArray[Math.floor(Math.random() * randomArray.length)]
+        return random;
     }
 function playRound(computerSelection, playerSelection) {
     score = score;
     pcScore = pcScore;
+     playerSelection = prompt("Rock,paper,scissors?");
+     computerSelection = computerPlay();
 
-if (computerSelection === playerSelection) {
+if (computerSelection == playerSelection) {
     alert("Tie");
     }
  else if (computerSelection == "rock" && playerSelection == "scissors") {
-        (computerSelection == "scissors" && playerSelection == "paper") ||
-        (computerSelection == "paper" && playerSelection == "rock") 
-        pcScore =+ 1;
+        (computerSelection == "paper" && playerSelection == "rock") &&
+        (computerSelection == "scissors" && playerSelection == "paper") 
+        pcScore = pcScore + 1;
         alert("PC wins");
 }
 else if (computerSelection == "rock" && playerSelection == "paper")  {
-(computerSelection == "paper" && playerSelection == "scissors") ||
-(computerSelection == "rock" && playerSelection == "paper")
-     score =+ 1;
+(computerSelection == "paper" && playerSelection == "scissors") &&
+(computerSelection == "scissors" && playerSelection == "rock")
+     score = score + 1;
     alert("You win");
 }
 
@@ -31,12 +34,24 @@ else if (computerSelection == "rock" && playerSelection == "paper")  {
 function game() {
     for (let i = 0; i < 5; i++) {
         playRound(playerSelection, computerPlay());
-        console.log("Your score is : " + score);
+        console.log("Your score is : " + score)
         console.log("Computer score is : " + pcScore);
      }
-     
-}
-console.log(computerPlay());
-console.log(playRound(playerSelection, computerSelection));
-game();
 
+
+    }
+    
+     function results() {
+        if (score === pcScore) {
+            console.log("You guys are tied!");
+        }
+        else if (score > pcScore) {
+            console.log("You won more rounds! Nice!")
+        }
+        else if(pcScore > score) {
+            console.log("Crap! AI is taking over!")
+        }
+     }
+console.log(computerPlay());
+game();
+results();;
